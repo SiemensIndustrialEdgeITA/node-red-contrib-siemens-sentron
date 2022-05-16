c6a05a9b0bc40a104f5126ae30becaa0d4d881b7
Nicolò Toscani <toscani.nicolo90@gmail.com>
2022-05-16 08:16:24 -0700

    getter.js enhancement

--- getter.js
+++ getter.js
@@ -49,112 +49,92 @@ module.exports = function(RED) {
         this.device = config.device;
         this.data = config.data;
         var node = this;
+        
+        const allowedModels = ["pac2200", "pac1200", "pac1600", "pac3100", "pac3200T", "pac4200", "pac5200", "sem3", "atc6300", "3va", "powercenter1000", "pac3120", "pac3220", "pac3200"]
+        
         node.on('input', function(msg) {
-        switch(msg.format) {
-            case "float32":
-                if(msg.model == "pac2200" || msg.model == "pac1200" || msg.model == "pac1600" || msg.model == "pac3100" || msg.model == "pac3200T" || msg.model == "pac4200"
-                   || msg.model == "pac5200" || msg.model == "sem3" || msg.model == "atc6300" || msg.model == "3va" || msg.model == "powercenter1000" || msg.model == "pac3120"|| msg.model == "pac3220" || msg.model == "pac3200" ){
-                   
-                   // Convert payload
-                   msg.payload = msg.payload.buffer.readFloatBE(0,4).toFixed(2);
-                } 
-                // Send payload
-                node.send(msg);
-            break;
-            case "float64":
-                if(msg.model == "pac2200" || msg.model == "pac1200" || msg.model == "pac1600" || msg.model == "pac3100" || msg.model == "pac3200T" || msg.model == "pac4200"
-                   || msg.model == "pac5200" || msg.model == "sem3" || msg.model == "atc6300" || msg.model == "3va" || msg.model == "powercenter1000" || msg.model == "pac3120"|| msg.model == "pac3220" || msg.model == "pac3200"){
-                   
-                   // Convert payload
-                   msg.payload = msg.payload.buffer.readFloatBE(0,8).toFixed(2);
-                } 
-                // Send payload
-                node.send(msg);
-            break;
-            case "uint16":
-                if(msg.model == "pac2200" || msg.model == "pac1200" || msg.model == "pac1600" || msg.model == "pac3100" || msg.model == "pac3200T" || msg.model == "pac4200"
-                   || msg.model == "pac5200" || msg.model == "sem3" || msg.model == "atc6300" || msg.model == "3va" || msg.model == "powercenter1000" || msg.model == "pac3120"|| msg.model == "pac3220" || msg.model == "pac3200"){
-                   
-                   // Convert payload
-                   msg.payload = msg.payload.buffer.readUInt16BE();
-                   
-                   // Check POC1000 status register
-                   if(msg.model == "powercenter1000" && msg.topic == "status"){
-                         
-                         if(msg.payload == 0){
-                         
-                            msg.deviceState = "Unknown";   
-                            
-                         }
-                         
-                         else if (msg.payload == 1){
-                         
-                            msg.deviceState = "Breaker OFF";
-                            
-                         }
-                         
-                         else if (msg.payload == 2){
-                         
-                            msg.deviceState = "Breaker ON";
-                            
-                         }
-                         else if (msg.payload == 3){
-                         
-                            msg.deviceState = "Breaker TRIP & level blocked";
-                            
-                         }
-                   }
-                   
-                }
-                // Send payload
-                node.send(msg);
-            break;
-            case "uint32":
-                if(msg.model == "pac2200" || msg.model == "pac1200" || msg.model == "pac1600" || msg.model == "pac3100" || msg.model == "pac3200T" || msg.model == "pac4200"
-                   || msg.model == "pac5200" || msg.model == "sem3" || msg.model == "atc6300" || msg.model == "3va" || msg.model == "powercenter1000" || msg.model == "pac3120"|| msg.model == "pac3220" || msg.model == "pac3200"){
-                
-                   // Convert payload
-                   msg.payload = msg.payload.buffer.readUInt32BE(0,4).toFixed(2);
-                }
-                // Send payload
-                node.send(msg);
-            break;
-            case "int32":
-                if(msg.model == "pac2200" || msg.model == "pac1200" || msg.model == "pac1600" || msg.model == "pac3100" || msg.model == "pac3200T" || msg.model == "pac4200"
-                   || msg.model == "pac5200" || msg.model == "sem3" || msg.model == "atc6300" || msg.model == "3va" || msg.model == "powercenter1000" || msg.model == "pac3120"|| msg.model == "pac3220" || msg.model == "pac3200"){
-                
-                   // Convert payload
-                   msg.payload = msg.payload.buffer.readInt32BE(0,4).toFixed(2);
-                }
-                // Send payload
-                node.send(msg);
-            break;
-            case "int64":
-                if(msg.model == "pac2200" || msg.model == "pac1200" || msg.model == "pac1600" || msg.model == "pac3100" || msg.model == "pac3200T" || msg.model == "pac4200"
-                   || msg.model == "pac5200" || msg.model == "sem3" || msg.model == "atc6300" || msg.model == "3va" || msg.model == "powercenter1000" || msg.model == "pac3120"|| msg.model == "pac3220" || msg.model == "pac3200"){
-                
-                    // Convert payload
-                   msg.payload = msg.payload.buffer.readBigInt64BE(0);
-                }
-                // Send payload
-                node.send(msg);
-            break;
-            case "double":
-                if(msg.model == "pac2200" || msg.model == "pac1200" || msg.model == "pac1600" || msg.model == "pac3100" || msg.model == "pac3200T" || msg.model == "pac4200"
-                   || msg.model == "pac5200" || msg.model == "sem3" || msg.model == "atc6300" || msg.model == "3va" || msg.model == "powercenter1000" || msg.model == "pac3120"|| msg.model == "pac3220" || msg.model == "pac3200"){
-                
-                    // Convert payload
-                    msg.payload = Number((fromDouble64IEEE(msg.payload.buffer) /1000).toFixed(3));
-                   
-                }
-                // Send payload
-                node.send(msg);
-            break;
-            
-            default:
-                node.warn("Getter not configured for this data format: (" + msg.format + ")." );
-                node.send(msg);
-        }
+        
+		if (!allowedModels.includes( msg.model)) {
+		        node.warn("Getter not configured for this model: (" + msg.model + ").");
+		        node.send(msg);
+		    } 
+
+		    switch (msg.format) {
+		        case "float32":
+		            // Convert payload
+		            msg.payload = msg.payload.buffer.readFloatBE(0, 4).toFixed(2);
+		            // Send payload
+		            node.send(msg);
+		            break;
+		        case "float64":
+		            // Convert payload
+		            msg.payload = msg.payload.buffer.readFloatBE(0, 8).toFixed(2);
+		            // Send payload
+		            node.send(msg);
+		            break;
+		        case "uint16":
+		            // Convert payload
+		            msg.payload = msg.payload.buffer.readUInt16BE();
+		            // Check POC1000 status register
+		            if (msg.model == "powercenter1000" && msg.topic == "status") {
+
+		                if (msg.payload == 0) {
+
+		                    msg.deviceState = "Unknown";
+
+		                }
+
+		                else if (msg.payload == 1) {
+
+		                    msg.deviceState = "Breaker OFF";
+
+		                }
+
+		                else if (msg.payload == 2) {
+
+		                    msg.deviceState = "Breaker ON";
+
+		                }
+		                else if (msg.payload == 3) {
+
+		                    msg.deviceState = "Breaker TRIP & level blocked";
+
+		                }
+		            }
+
+		            // Send payload
+		            node.send(msg);
+		            break;
+		        case "uint32":
+		            // Convert payload
+		            msg.payload = msg.payload.buffer.readUInt32BE(0, 4).toFixed(2);
+		            // Send payload
+		            node.send(msg);
+		            break;
+		        case "int32":
+		            // Convert payload
+		            msg.payload = msg.payload.buffer.readInt32BE(0, 4).toFixed(2);
+		            // Send payload
+		            node.send(msg);
+		            break;
+		        case "int64":
+		            // Convert payload
+		            msg.payload = msg.payload.buffer.readBigInt64BE(0);
+		            // Send payload
+		            node.send(msg);
+		            break;
+		        case "double":
+		            // Convert payload
+		            msg.payload = Number((fromDouble64IEEE(msg.payload.buffer) / 1000).toFixed(3));
+		            // Send payload
+		            node.send(msg);
+		            break;
+
+		        default:
+		            node.warn("Getter not configured for this data format: (" + msg.format + ").");
+		            node.send(msg);
+		    }
+        
  
         });
     }
